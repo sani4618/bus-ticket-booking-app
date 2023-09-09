66645627b4605154c2f5a3a94025ebfac2a8efdb
sani4618 <sanijoseph2018@gmail.com>
2023-09-09 22:08:10 +0530

    updation of bus ticket booking app

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import Register from './components/Register';
+import Search from './components/Search';
+import Delete from './components/Delete';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-     
-<Home/>
-<Register/>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" exact element={<Home/>}/>
+    <Route path="/register" exact element={<Register/>}/>
+    <Route path="/search" exact element={<Search/>}/>
+    <Route path="/delete" exact element={<Delete/>}/>
 
-    </div>
+   </Routes>
+   </BrowserRouter>
   );
 }
 
